9be9b09f121e5afceb53c77fc462bf63b87bc5fb
Arturo Filastò <arturo@filasto.net>
2022-11-10 10:59:33 +0100

    Ensure output_dir exists

--- sync_db_ip.py
+++ sync_db_ip.py
@@ -22,7 +22,9 @@ def get_latest_timestamp():
 def download_latest_dbip(cache_dir: Path) -> Path:
     current_ts = datetime.utcnow().strftime("%Y-%m")
     filename = f"dbip-country-lite-{current_ts}.mmdb.gz"
-    output_path = cache_dir / "dbip-country-lite" / filename
+    output_dir = cache_dir / "dbip-country-lite"
+    output_dir.mkdir(parents=True, exists_ok=True)
+    output_path = output_dir / filename
     print(f"   downloading latest db IP file to {output_path}")
     with requests.get(
         f"https://download.db-ip.com/free/{filename}", stream=True
