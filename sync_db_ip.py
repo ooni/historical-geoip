8c79ec82279a39cbb1b681e1d4a22c0a2106228e
decfox <mehul@openobservatory.org>
2026-02-02 13:16:48 +0530

    feat: add support to uplaod dbip-country-lite file to s3

--- sync_db_ip.py
+++ sync_db_ip.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from datetime import datetime
 from download_assets import list_all_ia_items
 
+import boto3
 import requests
 import internetarchive as ia
 
@@ -39,22 +40,34 @@ def download_dbip(cache_dir: Path, ts: str) -> Path:
     return output_path
 
 
+def upload_to_s3(prefix: str, filepath: Path, bucket_name: str, access_key: str, secret_key: str):
+    session = boto3.Session(
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+    )
+    s3_client = session.client('s3')
+    s3 = session.resource('s3')
+
+    with filepath.open("rb") as in_file:
+        s3_client.upload_fileobj(in_file, bucket_name, f'{prefix}/{filepath.name}')
+
+
 def upload_to_ia(identifier: str, filepath: Path, access_key: str, secret_key: str):
     print(f"   uploading db IP file {filepath} to {identifier}")
     files = {filepath.name: filepath.open("rb")}
     ia.upload(identifier, files=files, access_key=access_key, secret_key=secret_key)
 
 
-def maybe_sync(cache_dir: Path, secret_key: str, access_key: str):
+def maybe_sync(cache_dir: Path, current_ts: str, secret_key: str, access_key: str) -> str:
+    filepath = download_dbip(cache_dir, current_ts)
     latest_ts = get_latest_timestamp()
-    current_ts = datetime.utcnow().strftime("%Y%m")
     print(f"   latest available timestamp is {latest_ts}")
     if latest_ts != current_ts:
         print(f"[+] running sync to download {latest_ts}")
-        filepath = download_dbip(cache_dir, current_ts)
         upload_to_ia(
             "dbip-country-lite", filepath, secret_key=secret_key, access_key=access_key
         )
+    return filepath
 
 
 def main():
@@ -65,8 +78,19 @@ def main():
     cache_dir = Path("cache_dir")
     access_key = os.environ.get("IA_ACCESS_KEY", "")
     secret_key = os.environ.get("IA_SECRET_KEY", "")
+
+    s3_access_key = os.environ.get("S3_ACCESS_KEY", "")
+    s3_secret_key = os.environ.get("S3_SECRET_KEY", "")
+    s3_bucket_name = os.environ.get("S3_BUCKET_NAME", "")
+
     if access_key == "" or secret_key == "":
         print("WARNING IA_ACCESS_KEY or IA_SECRET_KEY are not set. Upload will fail")
+
+    if s3_access_key == "" or s3_secret_key == "":
+        print("WARNING S3_ACCESS_KEY or S3_BUCKET_NAME are not set. Upload will fail")
+    if s3_bucket_name == "":
+        s3_bucket_name = "ooni-data-eu-fra" # use ooni-data bucket as fallback
+
     if ts is not None:
         print(f"[+] downloading the following ts: {ts}")
         filepath = download_dbip(cache_dir, ts)
@@ -74,7 +98,9 @@ def main():
             "dbip-country-lite", filepath, secret_key=secret_key, access_key=access_key
         )
     else:
-        maybe_sync(cache_dir=cache_dir, access_key=access_key, secret_key=secret_key)
+        current_ts = datetime.now(datetime.timezone.utc).strftime("%Y%m")
+        filepath = maybe_sync(cache_dir=cache_dir, current_ts=current_ts, access_key=access_key, secret_key=secret_key)
+        upload_to_s3(prefix="dbip-country-lite", filepath=filepath, bucket_name=s3_bucket_name, access_key=s3_access_key, secret_key=s3_secret_key)
 
 
 if __name__ == "__main__":
